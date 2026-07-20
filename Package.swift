3e259ce92c2223956d3a2e254d9666dab71baebf
baileywickham <baileywickham@gmail.com>
2026-07-20 12:21:45 -0700

    Add Sparkle 2 dependency with Frameworks rpath

--- Package.swift
+++ Package.swift
@@ -4,11 +4,21 @@ import PackageDescription
 let package = Package(
     name: "ArtWall",
     platforms: [.macOS(.v14)],
+    dependencies: [
+        .package(url: "https://github.com/sparkle-project/Sparkle", from: "2.7.0")
+    ],
     targets: [
         .executableTarget(
             name: "ArtWall",
+            dependencies: [
+                .product(name: "Sparkle", package: "Sparkle")
+            ],
             path: "Sources/ArtWall",
-            exclude: ["Info.plist", "Resources"]
+            exclude: ["Info.plist", "Resources"],
+            linkerSettings: [
+                // Sparkle.framework is embedded at Contents/Frameworks by scripts/build.sh
+                .unsafeFlags(["-Xlinker", "-rpath", "-Xlinker", "@executable_path/../Frameworks"])
+            ]
         )
     ]
 )
